178ef3a00b664fc350bc6f3bbd2703246af67c4d
Greg Orvik <gvorvik@gmail.com>
2018-05-31 08:40:43 -0500

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!!</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+              <th>7</th>
+              <th>8</th>
+              <th>9</th>
+              <th>10</th>
+              <th>11</th>
+              <th>12</th>
+              <th>13</th>
+              <th>14</th>
+              <th>15</th>
+              <th>16</th>
+              <th>17</th>
+              <th>18</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+              <td><input type="text"/></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
