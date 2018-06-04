2df71cc12f548d082ad7f6cb59c951ce9aa5e48b
Greg Orvik <gvorvik@gmail.com>
2018-06-03 23:06:25 -0500

    add route for scores

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-const scores = [4,3,2,1];
+const scoreRouter = require('./routes/scoreRouter');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('build'));
 
-app.post('/api/score', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-});
+app.use('/api/score', scoreRouter);
+
 
 app.listen(PORT, () => {
     console.log(`App listening at ${PORT}`);
