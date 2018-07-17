35c70f41e240d4f3c8d39ac786203f67dbf738a0
Greg Orvik <gvorvik@gmail.com>
2018-07-17 17:25:24 -0500

    set req.user on successful login

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const pool = require('./modules/pool');
 const PORT = process.env.PORT || 5000;
 
 const scoreRouter = require('./routes/scoreRouter');
@@ -10,13 +11,18 @@ const courseRouter = require('./routes/courseRouter');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(passport.initialize());
+app.use(passport.session())
+require('./modules/passport')(passport, pool);
 app.use(express.static('build'));
 
-app.post('/api/login', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-})
+
+app.post('/api/login', passport.authenticate('local'),
+    (req, res) => {
+        console.log('req.user:', req.user);
+        res.send(req.user);
+    }
+);
 
 app.use('/api/score', scoreRouter);
 app.use('/api/course', courseRouter);
