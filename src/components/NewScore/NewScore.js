dbe932163f974ccca9e80a89c3a5e936304030fb
Greg Orvik <gvorvik@gmail.com>
2018-09-12 21:14:34 -0500

    develop the search component

--- src/components/NewScore/NewScore.js
+++ src/components/NewScore/NewScore.js
@@ -65,28 +65,34 @@ class NewScore extends Component {
 
     handleSubmit = () => {
         let totalScore = 0;
+
         if(this.state.date === '') {
             return alert('Please select a date');
         }
+
         if(Object.keys(this.props.scoreReducer).length !== this.props.holeInfo.length) {
             return alert('You need a score for every hole');
         }
+
         for (let score in this.props.scoreReducer) {
             if (this.props.scoreReducer[score] <= 0 || this.props.scoreReducer[score] === null || this.props.scoreReducer[score] === undefined) {
                 return alert('You cannot have a score below 1 on a hole');
             }
             totalScore = totalScore + this.props.scoreReducer[score];
         }
+
         let objectToSend = {
             scores: this.props.scoreReducer,
             courseID: this.state.selectedCourseId,
             date: this.state.date,
             totalScore,
         }
+        
         this.props.dispatch({
             type: SCORE_ACTIONS.SUBMIT_SCORE,
             payload: objectToSend,
         });
+
         this.props.history.push('/home');
     }
 
---
+++ src/components/Search/HoleHistory/HoleHistory.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class HoleHistory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    };
+
+    render() {
+        return <div>
+            <h1>Hole History</h1>
+        </div>
+    }
+}
+
+export default HoleHistory;
